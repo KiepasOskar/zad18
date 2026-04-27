85270aec21d85f20dc1d2e145223b00f7ab08841
KiepasOskar <kiepas.oskar@zst.radom.pl>
2026-04-27 09:09:07 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+
+        $a = 10;
+
+        echo $a; 
+        echo "<br>";
+        echo $a * -1;
+        echo "<br>";
+        echo $a + 3;
+        echo "<br>";
+        echo $a - 5;
+        echo "<br>";
+        echo $a * 2;
+        echo "<br>";
+        echo $a / 5;
+        echo "<br>";
+        echo $a % 2;
+        echo "<br>";
+        echo pow($a, 2);
+        echo "<br>";
+        echo "Wartość zmiennej \$a wynosi: $a i już się nie zmieni.";
+    ?>
+</body>
+</html>
